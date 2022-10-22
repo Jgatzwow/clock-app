8cff7e8e4616192c518d6a52da26fa1db56b4034
MSokol <mihasokol1993@gmail.com>
2022-10-22 14:00:53 +0300

    did a clock task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    const date = new Date()
+    const currentDate = new Date().toDateString()
+    const initTime = date.toLocaleTimeString('en-US', {hour12: false})
+    const [time, setTime] = useState(initTime)
+
+    useEffect(()=> {
+
+        setInterval(()=> {
+            setTime((new Date().toLocaleTimeString('en-US', {hour12: false})))
+        }, 1000)
+
+    }, [])
+
+    return (
+
+        <div className="clock__App">
+                {time}
+            <div>
+                {currentDate}
+            </div>
+        </div>
+    );
 }
 
-export default App;
+
